e7d26ef22752e53f9b52d00261471934ede7416a
Brian Park <briancpark@berkeley.edu>
2020-04-12 12:42:54 +0900

    cleaned up directories

---
+++ Populating Dots/sketch.js
@@ -0,0 +1,45 @@
+var spot = {
+        x : 500,
+        y : 500,
+        };
+
+var color = {
+    r: 0, 
+    g: 50,
+    b: 0,
+}
+
+var size;
+
+function setup() {  
+    createCanvas(1000, 1000);
+    
+    frameRate(60);    
+    background(0);
+  
+    
+    
+}
+
+
+
+function draw() {
+    
+    size = random(50, 100);
+    color.r = random(0, 255);
+    spot.x = random(0, 1000);
+    spot.y = random(0, 1000);
+    noStroke();
+    fill(0, color.r, 0, 50);
+    
+    ellipse(spot.x, spot.y, size, size);
+    
+}
+
+/*function mousePressed() {
+   
+}
+*/
+
+
+
