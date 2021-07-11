69599b26f467bac3c45650c6a7e5ade1c00a2b58
Brian Park <briancpark@berkeley.edu>
2021-07-11 00:52:28 -0400

    add random vector sketch

--- vectors/sketch.js
+++ vectors/sketch.js
@@ -3,10 +3,11 @@ let walker;
 function setup() {
     createCanvas(400, 400);
     walker = new Walker(200, 200);
-    background(0);
+
 }
 
 function draw() {
+    background(0);
     walker.update();
     walker.show();
 }
--- vectors/walker.js
+++ vectors/walker.js
@@ -1,16 +1,20 @@
 class Walker {
     constructor(x, y) {
         this.pos = createVector(x, y);
+        this.vel = p5.Vector.random2D();
+        this.vel.mult(random(3));
     }
 
     update() {
-        this.pos.x += random(-1, 1);
-        this.pos.y += random(-1, 1);
+        this.pos.add(this.vel)
     }
 
     show() {
-        stroke(floor(random(255)), floor(random(255)), floor(random(255)), floor(random(255)));
+        //stroke(floor(random(255)), floor(random(255)), floor(random(255)), floor(random(255)));
+        stroke(255);
         strokeWeight(2);
-        point(this.pos.x, this.pos.y);
+        fill(255, 100)
+        ellipse(this.pos.x, this.pos.y, 32)
+        //point(this.pos.x, this.pos.y);
     }
 }
