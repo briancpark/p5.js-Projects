c7ece8103cd710c8209311ad1625989303a54798
Brian Park <briancpark@berkeley.edu>
2021-07-19 23:51:44 -0400

    simulating forces

--- vectors/sketch.js
+++ vectors/sketch.js
@@ -8,6 +8,16 @@ function setup() {
 
 function draw() {
     background(0);
+
+    if (mouseIsPressed) {
+        let wind = createVector(0.1, 0);
+        walker.applyForce(wind);
+    }
+
+    let gravity = createVector(0, 0.2);
+    walker.applyForce(gravity);
+
     walker.update();
+    walker.edges();
     walker.show();
 }
